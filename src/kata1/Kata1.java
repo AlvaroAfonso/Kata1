94d8f6af160057087fd81163ef977c985e691564
Álvaro Javier Afonso López <alvaroafonso57@gmail.com>
2020-10-05 19:37:17 +0100

    Tercera versión de Kata1

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,17 +1,11 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
+import java.time.LocalDate;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1999, 3, 25);
-
-        Person person = new Person("Blas", date);
+        Person person = new Person("Álvaro", LocalDate.of(1999,3,25));
         System.out.println(person.getName() + " tiene " + person.getAge() + " años ");
     }
     
